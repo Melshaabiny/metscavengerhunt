6eba9c1695218ec804b789a965ce13a7ee441847
eChung00 <eChung00@citymail.cuny.edu>
2015-04-02 23:32:51 -0400

    login, logout functions and few register tests are added! doc, testing should be added later!

---
+++ document/source/UserAuthentication.rst
@@ -0,0 +1,5 @@
+User Authentication Documentation
+*********************************
+
+.. automodule:: /home/munjo5746/Desktop/web_dev/fork/csc473s15_methunt_main/met_hunt/user_auth/tests
+	:members:
--- met_hunt/user_auth/urls.py
+++ met_hunt/user_auth/urls.py
@@ -5,4 +5,5 @@ from user_auth import views
 urlpatterns = patterns('',
 		url(r'^login/$', views.login, name='login'),
 		url(r'^register/$', views.register, name='register'),
+		url(r'^logout/$', views.logout_user, name='logout'),
 )
