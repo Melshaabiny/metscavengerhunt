adf6c5061d05c546c657e16f412cd7601096016b
Bancroft Sparkes <bsparke00@citymail.cuny.edu>
2015-04-04 18:31:53 -0400

    add is_authenticated to home.views.py

--- met_hunt/home/views.py
+++ met_hunt/home/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your views here.
 def main(request):
 	user = None
-	if request.user:
+	if request.user.is_authenticated():
 		user = User.objects.get(username=request.user)
 	args = {'user':user}
 	return render_to_response('home/home.html', args)
