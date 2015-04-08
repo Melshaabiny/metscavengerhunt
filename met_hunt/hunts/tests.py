2346f46bf52aa2df49fbb8ca04d74e150451a893
Santiago Salas <ssalas00@citymail.cuny.edu>
2015-04-08 04:09:15 -0400

    started tests about to initialize fixtures

--- met_hunt/hunts/tests.py
+++ met_hunt/hunts/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+"""
+Unit tests for 'hunts' django application
+
+The list of tests are:
+
+"""
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from mock import MagicMock, Mock, patch
+from user_auth.views import login, logout_user, register
 
 # Create your tests here.
+class hunts_test(TestCase):
+	def setUp(self):
+		self.client = Client()
+
+	def test_hunts_status(self):
+		resp = self.client.get('/hunts/') #should be directory of welcom page
+		self.assertEqual(resp.status_code, 200)
+
+	
