cde658e95c3bcbc2c1a7ad2337ddf64ee565c5bd
eChung00 <eChung00@citymail.cuny.edu>
2015-04-09 15:55:57 -0400

    more tests add, existing tests modified using mock

--- met_hunt/user_auth/tests.py
+++ met_hunt/user_auth/tests.py
@@ -8,13 +8,14 @@ The list of tests are :
 	- **test_register_user()** checks if **RegisterForm.register_user()** gets called when **user_auth.views.register** gets POST data.
 """
 
-from django.test import TestCase, Client
+from django.test import Client
 from django.contrib.auth.models import User
 from mock import MagicMock, Mock, patch
 from user_auth.views import login, logout_user, register
-
+from django.http import QueryDict
+import unittest
 # Create your tests here.
-class user_authTest(TestCase):
+class user_authTest(unittest.TestCase):
 
 	def setUp(self):
 		"""
@@ -30,77 +31,158 @@ class user_authTest(TestCase):
 		"""
 		User.objects.all().delete() # clean up the user database.
 
+
+###################################################################################
+###############Test Register
+
 	def test_register_status_code(self):
+		"""
+		**test_register_status_code()** checks if a user access to the register page, the status_code
+		is 200.
+		"""
 		# create client.
 		response = self.client.get('/user_auth/register/')
 		self.assertEqual(response.status_code, 200)
 
-	def test_login_status_code(self):
-		response = self.client.get('/user_auth/login/')
-		self.assertEqual(response.status_code, 200)
 
-	def test_profile_status_code(self):
-		response = self.client.get('/user_auth/login/')
-		self.assertEqual(response.status_code, 200)
 
-	def test_register(self):
+	def test_RegisterForm_get(self):
 		"""
-		**test_views_register()** checks if the function is called if anybody accessed the url **/user_auth/register/**.
-		This test should be considered as different test with **test_register_status_code()** since this function just
-		checks if the httpresponse returns the status_code 200.
+		**test_RegisterForm()** checks when request.method to **user_auth.views.register()** is
+		GET, the RegisterForm is properly instantiated. 
 		"""
 
-		with patch('user_auth.views.register') as reg:
-			# access the url.
+		# get method.
+		with patch('user_auth.views.RegisterForm') as reg:
 			self.client.get('/user_auth/register/')
-			# check if the function gets called.
-			reg.called
+			# check if RegisterForm is called with post method.
+			self.assertTrue(reg.called)
 
-	def test_login(self):
+	def test_RegisterForm_post(self):
 		"""
-		**test_views_login()** checks if the function is called if anybody accessed the url **/user_auth/login/**.
+		**test_RegisterForm()** checks when request.method to **user_auth.views.register()** is
+		POST, the RegisterForm is properly instantiated with appropriate data. This test only 
+		checks if the RegisterForm is called with the POST data passed into register().
+		"""
+		# post method.
+		with patch('user_auth.views.RegisterForm') as reg:
+			reg.return_value = MagicMock() # only checking if the RegisterForm is called with
+									  # proper data.
+			reg.return_value.is_valid.return_value = False # this makes form.is_valid() returns False in views.py.
+			args = {'user_name':'user1', 'password':'password'}
+			self.client.post('/user_auth/register/', args)
+			# check if RegisterForm is called with post method.
+			reg.assert_called_with(QueryDict('user_name=user1&password=password'))
+
+	@unittest.expectedFailure
+	def test_RegisterForm_invalid(self):
+		"""
+		**test_RegisterForm_validation()** tests if it raise exception when the form is not valid.
+		The attributes user_name and password are required. So if the registering user does not pass 
+		these two information, the form is not valid.
+		"""
+		# if the form is not valid, then User.objects.create_user would not be called.
+		with patch('user_auth.views.User.objects.create_user') as c_user:
+			with patch('user_auth.views.UserInfo.objects.create') as c_userinfo:
+				user = c_user.return_value.save = MagicMock()
+				userinfo = c_userinfo.return_value.save = MagicMock()
+				args = {'password':'password'} # not passing username.
+				res = self.client.post('/user_auth/register/', args)
+
+				# user.save() and userinfo.save() must not be called since the form is not valid.
+				self.assertTrue(user.save.called or userinfo.save.called)
+
+	@unittest.expectedFailure
+	def test_register_notUnique_username(self):
+		"""
+		**test_register_notUnique_username()** tests if a user trying to register with an username 
+		that is already exists in the database. This test should be failed since our website not allow
+		two users with same username.
+		"""
+
+		username = 'duplicate_username'
+		password = 'password'
+
+		# create user.
+		user = User.objects.create_user(username=username, password=password)
+		args = {'user_name':username, 'password':password}
+		self.client.post('/user_auth/register/', args) # this will raise exception.
+		User.objects.get(username=username).delete()
+
+###################################################################################
+###############Test login_user
+
+	def test_login_status_code(self):
+		"""
+		**test_login_status_code()** checks if a user access to the login page, the status_code
+		is 200.
+		"""
+		response = self.client.get('/user_auth/login/')
+		self.assertEqual(response.status_code, 200)
+
+
+	def test_LogInForm_get(self):
+		"""
+		**test_loginForm()** checks if the LogInForm is instantiated when an user access to login page.
 		This test should be considered as different test with **test_login_status_code()** since this function just
 		checks if the httpresponse returns the status_code 200. 
 		"""
 
-		with patch('user_auth.views.login') as login:
+		with patch('user_auth.views.LogInForm') as form:
 			# access the url.
+			# LogInForm is mocked.
 			self.client.get('/user_auth/login/')
-			login.called
+			form.assert_called_with()
 
-	def test_login_with_post(self):
+	def test_LogInForm_post(self):
 		"""
-		**test_login_with_post()** tests the **login()** methods with POST data. 
-		"""
-	def test_RegisterForm(self):
-		"""
-		**test_RegisterForm()** checks if request.method to **user_auth.views.register()** is POST or 
-		GET, the form is properly instantiated. 
+		**test_LogInForm_post()** tests if the LogINForm is properly instantiated with POST data.
+		If the post data looks like {'user_name':'test_user', 'password':'password'}, then the LogInForm
+		must be called with the argument **QueryDict('user_name=test_user&password=password')**.
 		"""
 
-		with patch('user_auth.views.RegisterForm') as reg:
-			args = {'user_name':'user1', 'password':'password'}
-			self.client.post('/user_auth/register/', args)
-			# check if RegisterForm is called with post method.
-			reg.called
+		args = {'user_name':'test_user', 'password':'password'}
+		with patch('user_auth.views.LogInForm') as form:
+			# make the function not to log the user in.
+			form.return_value.is_valid = MagicMock(return_value=False)
+			self.client.post('/user_auth/login/', args)
+			form.assert_called_with(QueryDict('user_name=test_user&password=password'))
 
-		with patch('user_auth.views.RegisterForm') as reg:
-			self.client.get('/user_auth/register/')
-			# check if RegisterForm is called with post method.
-			reg.called
-
-	def test_register_user(self):
+	@unittest.expectedFailure
+	def test_login_user_invalid_username(self):
 		"""
-		**test_register_user()** check if the function **RegisterForm.register_user()** 
-		is called when POST data is sent to **user_auth.views.register()** function.
+		**test_login_user_invalid_username()** tests if an user tries to login with invalid username such as
+		non-exist username.
 		"""
 
-		# mock register_user function.
-		with patch('user_auth.views.RegisterForm.register_user') as reg:
-			reg.return_value = None
-			# The function gets called when user_auth.register() gets POST data.
-			response = self.client.post('/user_auth/register/', {'user_name':'test_user', 'password':'test1_user'})
-			reg.assert_called_with()
+		args = {'user_name':'user', 'password':'password'}
+		if len(User.objects.get(username=args['user_name'])):
+			User.objects.get(username=args['user_name']).delete()
+
+		self.client.post('/user_auth/login/', args) # This will throw exception.
+
+
+
+	def test_profile_status_code(self):
+		"""
+		**test_profile_status_code()** checks if a user access to the profile page, the status_code
+		is 200.
+		"""
+		response = self.client.get('/user_auth/login/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_edit_status_code(self):
+		"""
+		**test_edit_status_code()** checks if a user access to the edit page, the status_code equals to 200.
+		"""
+		response = self.client.get('/user_auth/edit/')
+		self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
 
 
 	# def test_auth_forms_register_user(self):
--- met_hunt/user_auth/urls.py
+++ met_hunt/user_auth/urls.py
@@ -3,7 +3,7 @@ from user_auth import views
 
 
 urlpatterns = patterns('',
-		url(r'^login/$', views.login, name='login'),
+		url(r'^login/$', views.login_user, name='login'),
 		url(r'^register/$', views.register, name='register'),
 		url(r'^logout/$', views.logout_user, name='logout'),
 		url(r'^profile/$', views.profile, name='profile'),
