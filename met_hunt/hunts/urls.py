ae84da6feeca9c6dbe40d3e5733a1db6cee175b2
Moustafa Elshaabiny <moustafa.elshaabiny@gmail.com>
2015-04-12 23:55:38 -0400

    pass user input to render_result and successfully verify versus itemid

--- met_hunt/hunts/urls.py
+++ met_hunt/hunts/urls.py
@@ -5,12 +5,13 @@ from hunts import views
 
 urlpatterns = patterns('',
  
-	url(r'^getdata$',		views.getData, 			name='hunt_getData'),
+	url(r'^getdata$',		views.get_data, 			name='hunt_get_data'),
 	url(r'^welcome$',		views.render_hunt, 		name='hunt_welcome'),
 	url(r'^clue$', 		views.render_clue, 			name='rend_clue'),
     url(r'^verify$', 		views.render_verify, 	name='rend_verify'),
 	url(r'^result$', 		views.render_result, 	name='rend_result'),
 	url(r'^correct$', 	views.render_correct, 		name='rend_correct'),
+	url(r'^next$', 		views.next_proc, 				name='next_proc'),	
 	url(r'^incorrect$', 	views.render_incorrect, name='rend_incorrect'),
 	url(r'^congrats$',	views.render_congrats, 		name='rend_congrats'),
 
