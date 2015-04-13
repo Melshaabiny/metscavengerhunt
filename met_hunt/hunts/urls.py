ee7f1c0604cd30ae2c2b487401b1208d81ba60f6
Moustafa Elshaabiny <melshaa00@citymail.cuny.edu>
2015-04-13 14:54:11 -0400

    Get hunt id using url and pass it to get_data(). Make docstring.

--- met_hunt/hunts/urls.py
+++ met_hunt/hunts/urls.py
@@ -5,10 +5,10 @@ from hunts import views
 
 urlpatterns = patterns('',
  
-	url(r'^getdata$',		views.get_data, 			name='hunt_get_data'),
-	url(r'^welcome$',		views.render_hunt, 		name='hunt_welcome'),
-	url(r'^clue$', 		views.render_clue, 			name='rend_clue'),
-    url(r'^verify$', 		views.render_verify, 	name='rend_verify'),
+	url(r'^getdata/(?P<id>[0-9]{3})/$', views.get_data, name='hunt_get_data'),
+	url(r'^welcome$', views.render_hunt, name='hunt_welcome'),
+	url(r'^clue$', views.render_clue, name='rend_clue'),
+    url(r'^verify$', views.render_verify, name='rend_verify'),
 	url(r'^result$', 		views.render_result, 	name='rend_result'),
 	url(r'^correct$', 	views.render_correct, 		name='rend_correct'),
 	url(r'^next$', 		views.next_proc, 				name='next_proc'),	
