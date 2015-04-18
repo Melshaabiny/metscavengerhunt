2a1f1b3d0fcd4096b882d5c2dfcafc6723eaab17
Moustafa Elshaabiny <moustafa.elshaabiny@gmail.com>
2015-04-18 13:59:51 -0400

    add (half) medival hunt to db. Style hunt htmls.

--- met_hunt/hunts/parse_json.py
+++ met_hunt/hunts/parse_json.py
@@ -11,15 +11,21 @@ hunt = Hunts(ID=1,Title=str(data[0]['fields']['Title']),
 # save it.
 hunt.save()
 
+hunt = Hunts(ID=2,Title=str(data[0]['fields']['Title']), 
+			Category=str(data[0]['fields']['Category']),
+			Start=str(data[0]['fields']['Start']))
+# save it.
+hunt.save()
+
 
 # save items.
-for i in xrange(1, 11):
+for i in xrange(1, 16):
 	print str(data[i]['pk']),str(data[i]['fields']['Category'])
 	item = Items(ID=str(data[i]['pk']),Category=str(data[i]['fields']['Category']))
 	# save it
 	item.save()
 
-for i in xrange(11, len(data)):
+for i in xrange(16, len(data)):
 	# get item data.
 	item = Items.objects.get(ID=str(data[i]['fields']['item']))
 
