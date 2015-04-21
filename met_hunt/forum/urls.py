ab88c289a261f8e9c7edcbc98b8617f2c5b51f27
Edward Chung <eChung00@citymail.cuny.edu>
2015-04-21 00:16:30 -0400

    modified unit test, modified forum

--- met_hunt/forum/urls.py
+++ met_hunt/forum/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'met_hunt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 	url(r'^thread/$', views.thread_main, name='thread_main'),
+	url(r'^thread/modern_thread/$', views.modern_thread, name='modern_thread'),
+	url(r'^thread/modern_thread/load_data/$', views.load_data, name='load_data'),
 )
--- met_hunt/forum/views.py
+++ met_hunt/forum/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response, redirect
-from forum.models import Thread
+from forum.models import Thread, QuestionAsked
+from django.http import JsonResponse
+
 # Create your views here.
 
 def thread_main(request):
@@ -10,3 +12,30 @@ def thread_main(request):
 	threads = Thread.objects.all()
 	args = {'threads':threads}
 	return render_to_response('forum/thread_main.html', args)
+
+def modern_thread(request):
+	thread = Thread.objects.get(type='modern')
+	args = {'total_post' : thread.total_post}
+	return render_to_response('forum/modern_thread.html', args)
+
+def load_data(request):
+	if request.method == "GET":
+		# instantiate the thread.
+		thread = Thread.objects.get(type="modern")
+		posts = QuestionAsked.objects.filter(thread=thread)
+		########## The json file always formatted as 
+				   # {
+				   # 'data' : [
+				   # 			 {'component':'value'}, ...
+				   # ]
+				   # }
+		data = {'json':[]}
+		for each in posts:
+			data['json'] += [{'title' : each.title,
+							  'description' : each.description,
+							  'when': each.when,
+							  'view' : each.view,
+							  'username' : each.user.username,
+							  'picture' : each.user.userinfo.picture.path}]
+
+		return JsonResponse(data)
