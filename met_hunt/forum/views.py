7cefde9bf6bbf1081b089750962e7a49a4ccf3d6
Edward Chung <munjo5746@gmail.com>
2015-04-25 15:29:52 -0400

    create post is implemented

--- met_hunt/forum/views.py
+++ met_hunt/forum/views.py
@@ -1,43 +1,55 @@
 from django.shortcuts import render_to_response, redirect
 from forum.models import Thread, QuestionAsked
 from django.http import JsonResponse
+from forum.models import Thread, QuestionAsked
+from django.contrib.auth.models import User
+import json
 
 # Create your views here.
 
 def thread_layout(request):
-	"""
-	This views.py is responsible for the view of main thread page. It should display
-	all type of threads with their description and total number of posts in each thread.
-	"""
-	return render_to_response('forum/thread_layout.html')
+    """
+    This views.py is responsible for the view of main thread page. It should display
+    all type of threads with their description and total number of posts in each thread.
+    """
+    return render_to_response('forum/thread_layout.html')
 
 def modern(request):
-	return render_to_response('forum/modern.html')
+    return render_to_response('forum/modern.html')
 
 def create(request):
-	return render_to_response('forum/create_post.html')
+    return render_to_response('forum/create_post.html')
 
 def load_data(request):
-	if request.method == "GET":
-		# instantiate the thread.
-		thread = Thread.objects.get(type="modern")
-		posts = QuestionAsked.objects.filter(thread=thread)
-		########## The json file always formatted as 
-				   # {
-				   # 'data' : [
-				   # 			 {'component':'value'}, ...
-				   # ]
-				   # }
-		data = {'json':[]}
-		for each in posts:
-			data['json'] += [{'title' : each.title,
-							  'description' : each.description,
-							  'when': each.when,
-							  'view' : each.view,
-							  'username' : each.user.username
-							  }]
-		return JsonResponse(data)
+    if request.method == "GET":
+        # instantiate the thread.
+        thread = Thread.objects.get(type="modern")
+        posts = QuestionAsked.objects.filter(thread=thread)
+        ########## The json file always formatted as 
+                   # {
+                   # 'data' : [
+                   #             {'component':'value'}, ...
+                   # ]
+                   # }
+        data = {'json':[]}
+        for each in posts:
+            data['json'] += [{'title' : each.title,
+                              'description' : each.description,
+                              'when': each.when,
+                              'view' : each.view,
+                              'username' : each.user.username,
+                              'thread' : 'modern',
+                              'logged_user' : request.user.username}]
+        return JsonResponse(data)
 
 def submit(request):
-	print request.body
-	return render_to_response('forum/modern.html')
+    """
+    This submit function takes the title and description of the post that
+    a user creates, sent by angularjs with $http.post method. This function
+    should handle data saving into thread model.
+    """
+    data = json.loads(request.body)
+    user = User.objects.get(username=data['username'])
+    thread = Thread.objects.get(type=data['thread'])
+    post = QuestionAsked.objects.create(user=user, thread=thread, title=data['title'], description=data['description'])
+    return render_to_response('forum/modern.html')
