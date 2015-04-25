c86a93d796398e37d911810a724738c550ea9de3
Edward Chung <eChung00@citymail.cuny.edu>
2015-04-24 23:36:46 -0400

    improvements on making post request from create post page

--- met_hunt/met_hunt/static/forum.js
+++ met_hunt/met_hunt/static/forum.js
@@ -1,6 +1,6 @@
-var modern_thread = angular.module('modern_thread', ['ngRoute', 'ngAnimate']);
+var modern_thread = angular.module('modern_thread', ['ngRoute', 'ngAnimate', 'ngCookies']);
 
-modern_thread.controller('modern_controller', function($scope, $http){
+modern_thread.controller('modern_controller', function($scope, $http, $cookies){
 	$http.get('load_data/')
 		 .success(function(data){
 		 	$scope.posts = data.json;
@@ -12,5 +12,18 @@ modern_thread.controller('modern_controller', function($scope, $http){
 		$scope.templateurl = "create/"
 		$scope.title = "Create Post"
 	}
+
+	$scope.submit = function(){
+		$http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+		$http({
+			method : "POST",
+			url : "submit/",
+			data : "test=data"
+		})
+			 .success(function(data){
+			 	alert("succeed!");
+			 });
+	}
+
 });
 
