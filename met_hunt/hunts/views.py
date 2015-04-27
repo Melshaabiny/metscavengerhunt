793560755a51646f751ba51d5a598225895b76ba
Moustafa Elshaabiny <melshaa00@citymail.cuny.edu>
2015-04-27 00:36:39 -0400

    add hints and images of medieval hunt to data based. Style hunt buttons

--- met_hunt/hunts/views.py
+++ met_hunt/hunts/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
-from hunts.models import Hunts, Items, Has, set_ItemsData, set_HuntsData, pop_item, verify_id
+from hunts.models import set_ItemsData, set_HuntsData, pop_item, verify_id
 from django.core.context_processors import csrf
 
+global temp
+
 def render_hunt(request, id):
-	"""
+    """
         * Gets triggered with the appropriate hunt id once a user chooses
         * a hunt from the categories' drop down menu
-        * Sets the hunt's data (title and starting location) and stores the items data in global var TEMP list 
-        * finally, it renders the welcome page 
+        * Sets the hunt's data (title and starting location) and stores the items data in global var TEMP list
+        * finally, it renders the welcome page
     """
-	global temp
-	hunt_dict = set_HuntsData(id)
-	temp = set_ItemsData(id)
-	return render_to_response("hunts/hunt.html", {"title": hunt_dict['hunt title'], "start_pt": hunt_dict['hunt start']})
+    global temp
+    hunt_dict = set_HuntsData(id)
+    temp = set_ItemsData(id)
+    return render_to_response("hunts/hunt.html", {"title": hunt_dict['hunt title'], "start_pt": hunt_dict['hunt start']})
 
 def next_proc(request):
     """
         * If the current item is the last one in the hunt, next_proc() redirect to render_congrats
         * Otherwise, it calls pop_item() to update the global list of items
-        	and redirects to render_clue()
+            and redirects to render_clue()
     """
     global temp
     temp = pop_item(temp)
     if len(temp) > 0:
         return redirect('rend_clue')
+    else:
+        return redirect('rend_congrats')
 
 def render_clue(request):
     """
@@ -39,12 +43,12 @@ def render_result(request):
         * based on the user input it either redirects to render_correct(), render_incorrect(), or render_congrats
     """
     if request.method == "POST":
-        
+
         usr_input_value = str(request.POST.get('input', ''))
-        
+
         if verify_id(usr_input_value) == True:
             global temp
-            if len(temp)==1:
+            if len(temp) == 0:
                 return redirect('rend_congrats')
             else:
                 return redirect('rend_correct')
@@ -73,11 +77,11 @@ def render_correct(request):
 def render_incorrect(request):
     """
         * render incorrect answer's page
-    """    
+    """
     return render_to_response("hunts/incorrect.html", {})
 
 def render_congrats(request):
     """
         * render congratulations page after finishing hunt
-    """   
+    """
     return render_to_response("hunts/congrats.html", {})
