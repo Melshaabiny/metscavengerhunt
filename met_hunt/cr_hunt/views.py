288159b513db889a9735858c8a666fe4f59ad687
Santiago Salas <santi.f.salas@gmail.com>
2015-04-27 15:58:49 -0400

    added models from hunts app

--- met_hunt/cr_hunt/views.py
+++ met_hunt/cr_hunt/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.shortcuts import redirect
+#from cr_hunt.models import 
+from django.core.context_processors import csrf
 # Create your views here.
+
+def render_main(request,id):
+	pass
