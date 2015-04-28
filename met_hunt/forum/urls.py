e86c8ca4e4897dd2431e07dd5aac4eed21e6d4c6
Santiago Salas <santi.f.salas@gmail.com>
2015-04-28 02:36:50 -0400

    merge

--- met_hunt/forum/urls.py
+++ met_hunt/forum/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+#from django.contrib import admin
 from forum import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'met_hunt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^thread/$', views.thread_main, name='thread_main'),
-	url(r'^thread/modern_thread/$', views.modern_thread, name='modern_thread'),
-	url(r'^thread/modern_thread/load_data/$', views.load_data, name='load_data'),
+    url(r'^thread/$', views.thread_layout, name='thread_layout'),
+    url(r'^thread/modern/$', views.modern, name='modern'),
+    url(r'^thread/load_data/$', views.load_data, name='load_data'),
+    url(r'^thread/create/$', views.create, name='create'),
+    url(r'^thread/submit/$', views.submit, name='submit'),
 )
