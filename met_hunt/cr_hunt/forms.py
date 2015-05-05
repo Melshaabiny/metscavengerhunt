b4f123fdfe558f48a0a75b0e42ad53254edfa929
Santiago Salas <ssalas00@citymail.cuny.edu>
2015-05-05 01:04:43 -0400

    fixed dropdown list in add items

--- met_hunt/cr_hunt/forms.py
+++ met_hunt/cr_hunt/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from cr_hunt.models import Items
 
-class ItemForm(forms.ModelForm):
-    clue = forms.CharField(label='Enter Clue:', max_length=250, help_text="Clue")
-    item = forms.ModelChoiceField(label='Add Item', queryset=Items.objects.all(), help_text="Item")
+class ItemForm(forms.Form):
+    clue = forms.CharField(label='Enter Clue:', max_length=250)
+    item = forms.ModelChoiceField(label='Add Item:', queryset=Items.objects.all(), help_text="Item")
 
-    class Meta:
-        model = Items
-        fields = ('clue', 'item')
+    
