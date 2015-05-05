bc47a75c111c3111f57bceedb775466316698d43
Santiago Salas <santi.f.salas@gmail.com>
2015-05-05 07:05:02 -0400

    added views

--- met_hunt/cr_hunt/urls.py
+++ met_hunt/cr_hunt/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
 	url(r'^ats$', views.render_ats, name='cr_ats'),
 	url(r'^proc_ts$', views.render_proc_ts, name = 'cr_proc_ts'),
     	url(r'^aitem$', views.render_aitem, name='cr_aitem'),
+        url(r'^proc_it$', views.render_proc_it, name = 'cr_proc_it'),
+	url(r'^end$', views.render_end, name = 'cr_end'),
 )
