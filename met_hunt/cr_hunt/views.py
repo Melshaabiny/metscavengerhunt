1c4fa186d03184c38ae1f795e1cc1b358511e54b
Santiago Salas <ssalas00@citymail.cuny.edu>
2015-05-12 13:33:57 -0400

    added unique hunt_id generator for cr hunts

--- met_hunt/cr_hunt/views.py
+++ met_hunt/cr_hunt/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
-from cr_hunt.models import add_hunt_its, add_hunt_has
+from cr_hunt.models import add_hunt_its, add_hunt_has, gen_hunt_id, get_cur_count
 from django.core.context_processors import csrf
 # Create your views here.
 
@@ -11,8 +11,9 @@ def render_main(request):
     if request.user.is_authenticated():
         global hunt_id
         hunt_id = request.user.username
+        hunt_id = gen_hunt_id(hunt_id)
         global i_counter
-        i_counter = 0
+        i_counter = get_cur_count(hunt_id)
         return render_to_response("cr_hunt/cr_hunt_main.html")
     else:
         return redirect('cr_error')
