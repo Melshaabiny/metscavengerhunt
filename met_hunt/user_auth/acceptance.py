1a31cb262992bf32dec4c8acddc5f7e2d4c3b79c
Edward Chung <munjo5746@gmail.com>
2015-05-14 02:53:53 -0400

    acceptance test added

--- met_hunt/user_auth/acceptance.py
+++ met_hunt/user_auth/acceptance.py
@@ -1,6 +1,13 @@
-
-
 from selenium import webdriver
+
+def login(browser):
+    browser.get('http://slowlybutconstantly.org/scav_hunt/user_auth/login/')
+    username_field = browser.find_element_by_name('user_name')
+    username_field.send_keys('user4')
+    password_field = browser.find_element_by_name('password')
+    password_field.send_keys('user')
+    return browser
+
 
 browser = webdriver.Firefox()
 browser.get('http://slowlybutconstantly.org/scav_hunt') # get the webpage.
@@ -27,6 +34,23 @@ try:
     password_field.send_keys('user')
 except:
     raise Exception("Login failed!")
-    
+
+############# Ancient Art
+browser.get('http://slowlybutconstantly.org/scav_hunt/hunts/welcome/1')
+assert 'Museum Hunters' in browser.title
+input_clue = browser.find_element_by_tag_name('input')
+input_clue.click()
+
+
+############ Medieval Art
+browser.get('http://slowlybutconstantly.org/scav_hunt/hunts/welcome/2')
+input_clue = browser.find_element_by_tag_name('input')
+input_clue.click()
+
+############ Profile page
+# browser = login(browser)
+# profile = browser.find_element_by_id('profile')
+# profile.click()
+
 browser.quit()
 
